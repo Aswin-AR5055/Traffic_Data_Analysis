6bf5a9077b6010089773a6884d33e7a020b1215f
Aswin Raj <aswinraj2003@gmail.com>
2026-01-21 21:46:43 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -6,7 +6,7 @@ def train_model(data):
     x = data[['Hour', 'Month', 'Visibility_km', 'Temperature_C', 'Traffic_Signal', 'Is_Weekend']]
     y = data['Severity']
 
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
     model = DecisionTreeClassifier(random_state=42, max_depth=5, min_samples_split=10)
     model.fit(x_train, y_train)
